4e3603031c55dd0334eaa109d400e53b2347b1d0
TheShadowScout <kacper_chrz@wp.pl>
2022-12-13 10:04:56 +0100

    Modyfikacja navbara i footera

--- templates/nav.html.php
+++ templates/nav.html.php
@@ -2,8 +2,17 @@
 /** @var $router \App\Service\Router */
 
 ?>
-<ul>
-    <li><a href="<?= $router->generatePath('') ?>">Home</a></li>
-    <li><a href="<?= $router->generatePath('post-index') ?>">Posts</a></li>
-</ul>
+<style>
+    .navbar {
+        background-color: lightgray;
+    }
+</style>
+<div class="navbar">
+    Linke&reg;
+    <ul>
+        <li><a href="<?= $router->generatePath('') ?>">Strona główna</a></li>
+        <li><a href="<?= $router->generatePath('public-addLink') ?>">Dodaj link</a></li>
+        <li><a href="<?= $router->generatePath('public-login') ?>">Zaloguj sie</a></li>
+    </ul>
+</div>
 <?php

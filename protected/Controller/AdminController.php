41a16be786e04af857a9a31d91ec18b2974d68a4
TheShadowScout <kacper_chrz@wp.pl>
2022-12-15 12:27:19 +0100

    Zmiana nazwy szablonu sql

--- protected/Controller/AdminController.php
+++ protected/Controller/AdminController.php
@@ -4,12 +4,16 @@ namespace App\Controller;
 use App\Exception\NotFoundException;
 use App\Service\Router;
 use App\Service\Templating;
+use App\Model\User;
+use App\Model\Link;
 
 // Kontroler do którego ma się dostęp TYLKO po zweryfikowaniu kim się jest
 class AdminController
 {
     public function index(Templating $templating, Router $router): ?string
     {
+        $users = User::findAll();
+        $links = Link::findAll();
         $html = $templating->Render('admin/index.html.php', [
             'router' => $router
         ]);

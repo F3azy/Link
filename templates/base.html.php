4da9370be6bcff3cc34d7cb3aa9609acf5ce8aa8
Mariusz Lubowicki <Luberski@protonmail.com>
2022-12-18 19:24:00 +0100

    Add Tailwind support with simple styles.

--- templates/base.html.php
+++ templates/base.html.php
@@ -5,7 +5,91 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/assets/dist/style.min.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+
+    <style type="text/tailwindcss">
+        @tailwind base;
+      @tailwind components;
+      @tailwind utilities;
+      @layer base {
+        *,
+        *::before,
+        *::after {
+          @apply box-border antialiased;
+        }
+        body {
+          @apply w-full h-full p-0 m-0 text-neutral-900 bg-white;
+        }
+        h1 {
+          @apply text-5xl font-bold;
+        }
+        h2 {
+          @apply text-4xl font-bold;
+        }
+        h3 {
+          @apply text-3xl font-bold;
+        }
+        h4 {
+          @apply text-2xl font-bold;
+        }
+        h5 {
+          @apply text-xl font-bold;
+        }
+        h6 {
+          @apply text-lg font-bold;
+        }
+        a {
+          @apply no-underline cursor-pointer text-indigo-900;
+        }
+        a:hover,
+        a:active {
+          @apply underline;
+        }
+        button a:hover {
+          @apply no-underline;
+        }
+      }
+      @layer components {
+        .btn {
+          @apply px-4 py-2 text-center font-medium transition duration-150 ease-in-out cursor-pointer rounded-lg;
+        }
+        .btn:disabled,
+        .btn[disabled] {
+          @apply opacity-50 cursor-not-allowed;
+        }
+        .btn-primary {
+          @apply text-indigo-900 bg-indigo-100 shadow shadow-indigo-900/25 hover:bg-indigo-200;
+        }
+        .btn-secondary {
+          @apply text-neutral-900 bg-neutral-50 shadow shadow-black/25 hover:bg-neutral-200;
+        }
+        .btn-outline {
+          @apply text-neutral-900 bg-transparent border border-neutral-900 hover:bg-neutral-100;
+        }
+        .btn-outline-primary {
+          @apply text-indigo-900 bg-transparent border border-indigo-900 hover:bg-indigo-100;
+        }
+        .btn-danger {
+          @apply text-red-900 bg-red-50 shadow shadow-red-900/25 hover:bg-red-200;
+        }
+        .btn-success {
+          @apply text-green-900 bg-green-50 shadow shadow-green-900/25 hover:bg-green-200;
+        }
+        .btn-flat-secondary {
+          @apply text-neutral-900 bg-transparent hover:bg-neutral-200 border-none;
+        }
+        .btn-flat-primary {
+          @apply text-indigo-900 bg-transparent hover:bg-indigo-200 border-none;
+        }
+        .btn-flat-danger {
+          @apply text-red-900 bg-transparent hover:bg-red-200 border-none;
+        }
+        .input {
+          @apply w-full rounded-lg border border-neutral-300 bg-neutral-50 px-4 py-3 placeholder:text-neutral-400;
+        }
+      }
+    </style>
+
 </head>
 <body <?= isset($bodyClass) ? "class='$bodyClass'" : '' ?>>
 <style>
